d48555c8405b652b7b8ce3b1b3fde3195f2fcc52
Nikhil <nikiljana123@gmail.com>
2024-02-06 12:17:19 +0530

    added: Insert between nodes

--- LinkedList/src/com/bridgelabz/test/Test.java
+++ LinkedList/src/com/bridgelabz/test/Test.java
@@ -8,11 +8,17 @@ class MyNode<T>{
         this.data = data;
         this.next = null;
     }
+    public MyNode(T data, MyNode<T> next) {
+        this.data = data;
+        this.next = next;
+    }
 }
 
 class MyLinkedLists<T>{
     MyNode<T> head;
     MyNode<T> tail;
+    MyNode<T> temp;
+    int size=0;
     public MyLinkedLists() {
         this.head = null;
         this.tail = null;
@@ -25,6 +31,7 @@ class MyLinkedLists<T>{
         if (tail == null){
             tail = head;
         }
+        size++;
 
     }
     public void insertLast(T data){
@@ -35,6 +42,25 @@ class MyLinkedLists<T>{
         MyNode<T> node = new MyNode<>(data);
         tail.next = node;
         tail = node;
+        size++;
+    }
+
+    public void insertBetween(T data, int index){
+        if (index == 0){
+            insertFirst(data);
+            return;
+        }
+        if (index == size){
+            insertLast(data);
+            return;
+        }
+        temp = head;
+        for(int i = 1 ; i < index; i++){
+            temp = temp.next;
+        }
+        MyNode<T> node = new MyNode<T>(data, temp.next );
+        temp.next = node;
+        size++;
     }
 
 //    public void addNode(T data){
@@ -68,9 +94,10 @@ public class Test {
     public static void main(String[] args) {
         MyLinkedLists <Integer> integerList = new MyLinkedLists<>();
         integerList.insertFirst(56);
-        integerList.insertLast(30);
         integerList.insertLast(70);
+        integerList.insertBetween(30, 1);
         System.out.println("Linked List: ");
         integerList.displayList();
+
     }
 }
